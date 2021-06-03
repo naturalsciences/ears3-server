584994df21481dd77168fec5937aaa2f353d853e
Thomas Vandenberghe <tvandenberghe@naturalsciences.be>
2021-06-03 12:52:33 +0200

    new acquisition that stores everything in ears db

--- Acquisition_System/bin/amqdump.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-CONTAINER_CLASSPATH=conf
-
-for JAR_FILE in $( find lib -name "*.jar" ) ; do
-  CONTAINER_CLASSPATH=$CONTAINER_CLASSPATH:$JAR_FILE
-done
-
-java -cp "$CONTAINER_CLASSPATH" fr.ifremer.acquisition.BrokerDump $1 $2
-
--- Acquisition_System/bin/docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Starting mongo"
-/usr/bin/mongod -f /etc/mongod.conf &
-
-echo "Starting acquisition"
-
-cd /etc/nsa/
-./startup.sh all
