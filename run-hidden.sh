af1fb5fcbf4a79910cb0f090cbbbfa16e4498630
Thomas Vandenberghe <tvandenberghe@naturalsciences.be>
2021-06-08 13:32:40 +0200

    edge case where csr has no bounds when RV is not moving

--- run-hidden.sh
+++ run-hidden.sh
@@ -3,6 +3,6 @@ sudo docker system prune &&
 sudo docker-compose build &&
 sudo docker-compose up -d
 sleep 80
-echo Finished! Go to http://localhost:8181/ears
+echo Finished! Go to http://localhost/ears
 echo -ne '\007'
 
