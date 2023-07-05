dd874686c63de241f3aa29f82c33189d803751da
nvandensteen <nvandensteen@naturalsciences.be>
2023-07-05 11:47:57 +0200

    [ENH] run.sh checks if port is used -> kill

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+read -r pPID pNAME <<< $(sudo netstat -tulpn | grep -w ':80' | awk '{print $7;}' | cut -f1,2 -d/ --output-delimiter=" ")
+if [[ $pNAME != "" ]] && [[ $pPID != "" ]] && [[ $pNAME != "docker-proxy" ]]; then
+    echo "Killing $pPID: $pNAME"
+    sudo kill $pPID;
+fi;
+
 sudo docker kill ears-server_acquisition ears-server_tomcat ears-server_mysql ears-server_postgres
-sudo kill $(sudo netstat -tulpn | grep -w ':80' | awk '{print $7;}' | cut -f1 -d/)
 sudo docker-compose build &&
 sudo docker-compose up -d
 sleep 60
