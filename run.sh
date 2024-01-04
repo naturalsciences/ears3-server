c91dbbc3b4105dba90fa269210447c57cdf91e19
Thomas Vandenberghe <thomas.vdberghe@gmail.com>
2024-01-04 11:56:03 +0100

    upgrade ears3Nav to J11

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ if [[ $pNAME != "" ]] && [[ $pPID != "" ]] && [[ $pNAME != "docker-proxy" ]]; th
 fi;
 
 sudo docker kill ears-server_acquisition ears-server_tomcat ears-server_mysql ears-server_postgres
-sudo docker-compose build &&
-sudo docker-compose up -d
+sudo docker compose --profile embedded_db build &&
+sudo docker compose --profile embedded_db up -d
 sleep 60
 cd Acquisition_System/techsas-run/
 nohup java -Xms512m -Xmx2g -jar acquisition-launcher-1.1.0-SNAPSHOT.jar 2>&1 &
