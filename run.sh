4c76349081f9dd87723a2db31525eff43bf74267
jrymenans <jrymenans@naturalsciences.be>
2024-03-25 11:59:49 +0100

    Added unique constraint to events table and commented out the acquisition

--- run.sh
+++ run.sh
@@ -9,9 +9,9 @@ fi;
 sudo docker kill ears-server_acquisition ears-server_tomcat ears-server_mysql ears-server_postgres
 sudo docker compose --profile embedded_db build &&
 sudo docker compose --profile embedded_db up -d
-sleep 60
-cd Acquisition_System/techsas-run/
-nohup java -Xms512m -Xmx2g -jar acquisition-launcher-1.1.0-SNAPSHOT.jar 2>&1 &
+#sleep 60
+#cd Acquisition_System/techsas-run/
+#nohup java -Xms512m -Xmx2g -jar acquisition-launcher-1.1.0-SNAPSHOT.jar 2>&1 &
 sleep 40
 
 cd -
